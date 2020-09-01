9a3d5c434d189e4c34fc48c20654d46cf10f2eb5
Omar D <omar.drwch@gmail.com>
2020-09-01 16:04:13 +0200

    ...

--- rlxp/envs/__init__.py
+++ rlxp/envs/__init__.py
@@ -3,3 +3,4 @@ from .squareworld  import SquareWorld
 from .gridworld    import GridWorld
 from .mountaincar  import MountainCar
 from .chasingblobs import ChasingBlobs
+from .pball        import PBall, PBall2D
